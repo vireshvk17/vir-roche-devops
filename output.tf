3b830fefc4cd013440058707662dbfa600d24cca
vireshvk17 <viresh.kapoor@gmail.com>
2025-11-04 11:50:39 +0000

    update terraform

---
+++ output.tf
@@ -0,0 +1,17 @@
+#public ip of my ec2
+output "my-ec2-publicIP" {
+    value = aws_instance.example.public_ip
+  
+}
+
+# ec2 machine instance id
+
+output "my-ec2ID" {
+  value = aws_instance.example.id
+}
+
+#store data in local system
+resource "local_file" "ipdata" {
+  content  = aws_instance.example.public_ip
+  filename = "${path.module}/myip.txt"
+}
