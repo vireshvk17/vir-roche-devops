72266dfadbf277279616efa6cc146860c40d8f74
vireshvk17 <viresh.kapoor@gmail.com>
2025-11-05 05:07:46 +0000

    fixed ec2 tags and update output depneds

--- output.tf
+++ output.tf
@@ -14,4 +14,9 @@ output "my-ec2ID" {
 resource "local_file" "ipdata" {
   content  = aws_instance.example.public_ip
   filename = "${path.module}/myip.txt"
+
+  #making manual dependency --changes made in dryrun branch only
+  depends_on = [ aws_instance.example ]
+
+
 }
