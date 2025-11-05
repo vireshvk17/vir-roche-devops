5ed8e071bb4218e86605f9dc47f344cbf4619396
vireshvk17 <viresh.kapoor@gmail.com>
2025-11-05 10:35:52 +0000

    updated terraform provisioner woth custom private key

--- ec2.tf
+++ ec2.tf
@@ -9,5 +9,24 @@ resource "aws_instance" "example" {
   tags = {
     "Name" = var.vm-name     #"viresh-vm-1"
   }
-
+#provisioner
+provisioner "remote-exec" {
+  inline = [ 
+    "sudo dnf install git httpd -y",
+    "mkdir -p hello/terraform"
+   ]
 }
+
+connection {
+  
+  type = "ssh"
+  user = "ec2-user"
+  host = self.public_ip
+  timeout = "5m"
+
+  #content of private key data
+  private_key = tls_private_key.rsa-4096-example.private_key_pem
+}
+
+} 
+
