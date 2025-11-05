34dbc3b9c5304052a8baa1a85e4952c45b7855d8
vireshvk17 <viresh.kapoor@gmail.com>
2025-11-05 07:28:53 +0000

    implementing variables

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,12 @@
 # creating ec2 vm 
 resource "aws_instance" "example" {
-  ami           = "ami-0a25a306450a2cba3"
-  instance_type = "t2.nano"
-  key_name      = "roche-key"
+  #ami           = "ami-0a25a306450a2cba3"
+  ami = var.viresh-ami-id # variable id came from variables.tf. the value will be asked during run time. 
+  instance_type = var.vm-size    # "t2.nano"
+  key_name      = var.ec2-key-name      #"roche-key"
   # changing tags_all to tags 
   tags = {
-    "Name" = "viresh-vm-1"
+    "Name" = var.vm-name     #"viresh-vm-1"
   }
 
 }
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "viresh-ami-id" {
+    type = string
+    description = "this is my sydney aws region ami id"
+    # default = "ami-0a25a306450a2cba3"
+  
+}
+
+variable "vm-name" {
+    type = string
+    description = "my aws vm name"
+
+}
+
+variable "vm-size" {
+    type = string
+  
+}
+
+variable "ec2-key-name" {
+    type = string
+    #sensitive = true
+}
